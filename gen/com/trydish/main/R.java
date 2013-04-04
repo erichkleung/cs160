0232aa0c53a45d72264d04e2c7677d6feac425fc
Brett Johnson <brettjohnson837@gmail.com>
2013-04-04 01:04:09 -0700

    Added placeholder button and spinner

--- gen/com/trydish/main/R.java
+++ gen/com/trydish/main/R.java
@@ -8,36 +8,42 @@
 package com.trydish.main;
 
 public final class R {
+    public static final class array {
+        public static final int distance_array=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int distance_spinner=0x7f080000;
+        public static final int menu_settings=0x7f080001;
     }
     public static final class layout {
         public static final int activity_find_home=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_review_home=0x7f030002;
-        public static final int activity_settings=0x7f030003;
+        public static final int activity_home_food_list=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_review_home=0x7f030003;
+        public static final int activity_settings=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_home_food_list=0x7f060000;
-        public static final int activity_home_screen=0x7f060001;
-        public static final int activity_review_home=0x7f060002;
-        public static final int activity_settings=0x7f060003;
+        public static final int activity_home_food_list=0x7f070000;
+        public static final int activity_home_screen=0x7f070001;
+        public static final int activity_review_home=0x7f070002;
+        public static final int activity_settings=0x7f070003;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int find=0x7f040006;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int review=0x7f040007;
-        public static final int settings=0x7f040008;
-        public static final int title_activity_home_screen=0x7f040003;
-        public static final int title_activity_review_home=0x7f040004;
-        public static final int title_activity_settings=0x7f040005;
+        public static final int app_name=0x7f050000;
+        public static final int find=0x7f050006;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int review=0x7f050007;
+        public static final int search=0x7f050009;
+        public static final int settings=0x7f050008;
+        public static final int title_activity_home_screen=0x7f050003;
+        public static final int title_activity_review_home=0x7f050004;
+        public static final int title_activity_settings=0x7f050005;
     }
     public static final class style {
         /** 
@@ -61,10 +67,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/com/trydish/find/FindHome.java
+++ src/com/trydish/find/FindHome.java
@@ -5,17 +5,50 @@ import com.trydish.main.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FindHome extends Fragment {
+	
+	//Keep track of what distance user has selected from drop down menu. Saving now b/c likely later passed to other funtion 
+	private String searchDistance = "1 Mile";
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.activity_find_home,
 				container, false);
+		
+		//Note we have to call findViewById on the view b/c we are not in an Activity
+		Spinner distanceSpinner = (Spinner) view.findViewById(R.id.distance_spinner);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(view.getContext(), R.array.distance_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		distanceSpinner.setAdapter(adapter);
+		
+		//Create a new OnItemSelectedListener for the Spinner using anonymous class to define necessary methods
+		OnItemSelectedListener listener = new OnItemSelectedListener() {
+			
+			//comment
+			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+				String itemSelected = (String) parent.getItemAtPosition(pos);
+				searchDistance = itemSelected;
+				Log.d("FindHome", searchDistance);
+				//note that when this tab is selected, this method is executed
+			}
+			
+			//comment
+			public void onNothingSelected(AdapterView<?> parent) {
+				
+			}
+		};
+		//Set the spinners listener
+		distanceSpinner.setOnItemSelectedListener(listener);
 
 		return view;
 
