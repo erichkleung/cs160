a14a2ea051dcaa512867cf9eaab2c2699499d97c
Anh Mai <abmai91@gmail.com>
2013-04-06 16:20:59 -0700

    Anh working on signup stuff

--- src/com/trydish/main/SignupAllergies.java
+++ src/com/trydish/main/SignupAllergies.java
@@ -2,6 +2,8 @@ package com.trydish.main;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.Menu;
 
 public class SignupAllergies extends Activity {
@@ -10,6 +12,19 @@ public class SignupAllergies extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_signup_allergies);
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(R.string.allergies_warning)
+			   .setTitle(R.string.important_allergies);
+		builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		});
+		
+		AlertDialog dialog = builder.create();
+		dialog.show();
 	}
 
 	@Override
