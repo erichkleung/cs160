b919069fdc4b569ca4c132ffdf6e76b9b32b558d
Brett Johnson <brettjohnson837@gmail.com>
2013-04-07 21:00:04 -0700

    changed grid spacing on home screen

--- src/com/trydish/find/ImageAdapter.java
+++ src/com/trydish/find/ImageAdapter.java
@@ -33,9 +33,9 @@ public class ImageAdapter extends BaseAdapter {
         ImageView imageView;
         if (convertView == null) {  // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
+            imageView.setLayoutParams(new GridView.LayoutParams(255, 200));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
+            imageView.setPadding(4, 4, 4, 4);
         } else {
             imageView = (ImageView) convertView;
         }
