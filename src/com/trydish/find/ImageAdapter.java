00bb6a0101438ce3be85e4146184dc8ffe967fee
Brett Johnson <brettjohnson837@gmail.com>
2013-04-08 01:46:57 -0700

    colors and pictures

--- src/com/trydish/find/ImageAdapter.java
+++ src/com/trydish/find/ImageAdapter.java
@@ -10,61 +10,52 @@ import android.widget.GridView;
 import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
-    private Context mContext;
+	private Context mContext;
 
-    public ImageAdapter(Context c) {
-        mContext = c;
-    }
+	public ImageAdapter(Context c) {
+		mContext = c;
+	}
 
-    public int getCount() {
-        return mThumbIds.length;
-    }
+	public int getCount() {
+		return mThumbIds.length;
+	}
 
-    public Object getItem(int position) {
-        return null;
-    }
+	public Object getItem(int position) {
+		return null;
+	}
 
-    public long getItemId(int position) {
-        return 0;
-    }
+	public long getItemId(int position) {
+		return 0;
+	}
 
-    // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {  // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(255, 200));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(4, 4, 4, 4);
-        } else {
-            imageView = (ImageView) convertView;
-        }
+	// create a new ImageView for each item referenced by the Adapter
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ImageView imageView;
+		if (convertView == null) {  // if it's not recycled, initialize some attributes
+			imageView = new ImageView(mContext);
+			imageView.setLayoutParams(new GridView.LayoutParams(255, 200));
+			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageView.setPadding(4, 4, 4, 4);
+		} else {
+			imageView = (ImageView) convertView;
+		}
 
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
-    }
+		imageView.setImageResource(mThumbIds[position]);
+		return imageView;
+	}
 
-    // references to our images
-    private Integer[] mThumbIds = {
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food,
-            R.drawable.food, R.drawable.food
-    };
+	// references to our images
+	private Integer[] mThumbIds = {
+			R.drawable.food, R.drawable.wings,
+			R.drawable.dimsum, R.drawable.burger,
+			R.drawable.food1, R.drawable.food, R.drawable.wings,
+			R.drawable.dimsum, R.drawable.burger,
+			R.drawable.food1, R.drawable.food, R.drawable.wings,
+			R.drawable.dimsum, R.drawable.burger,
+			R.drawable.food1, R.drawable.food, R.drawable.wings,
+			R.drawable.dimsum, R.drawable.burger,
+			R.drawable.food1, R.drawable.food, R.drawable.wings,
+			R.drawable.dimsum, R.drawable.burger,
+			R.drawable.food1
+	};
 }
