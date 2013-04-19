e98907e878d304b03ce4294bce2b45e2024b4f91
Kimberly White <Jon@Jon-PC.(none)>
2013-04-18 21:00:15 -0700

    basic checking on create account user/pass

--- src/com/trydish/main/SignupHome.java
+++ src/com/trydish/main/SignupHome.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupHome extends Activity {
 
@@ -18,6 +20,35 @@ public class SignupHome extends Activity {
 	}
 	
 	public void signupNext(View view) {
+		EditText userText = (EditText)(findViewById(R.id.signup_username));
+		EditText passText = (EditText)(findViewById(R.id.signup_password));
+		EditText confText = (EditText)(findViewById(R.id.signup_password_confirm));
+		
+		String user = userText.getText().toString();
+		String pass = passText.getText().toString();
+		String conf = confText.getText().toString();
+		
+		if (user.equals("")) {
+			Toast toast = Toast.makeText(this, "Please enter a username.", Toast.LENGTH_SHORT);
+			toast.show();
+			return;
+		} else if (pass.equals("")) {
+			Toast toast = Toast.makeText(this, "Please enter a password.", Toast.LENGTH_SHORT);
+			toast.show();
+			return;
+		} else if (conf.equals("")) {
+			Toast toast = Toast.makeText(this, "Please confirm your password.", Toast.LENGTH_SHORT);
+			toast.show();
+			return;
+		} else if (!(conf.equals(pass))) {
+			Toast toast = Toast.makeText(this, "Mismatched passwords, try again.", Toast.LENGTH_SHORT);
+			toast.show();
+			passText.setText("");
+			confText.setText("");
+			return;
+		}
+		
+		
 		Intent intent = new Intent(this, SignupAllergies.class);
 		startActivity(intent);
 	}
