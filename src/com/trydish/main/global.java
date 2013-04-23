76cbb9ad7d2cea661c35d987312b07ca2b0c554b
Kimberly White <Jon@Jon-PC.(none)>
2013-04-22 20:23:24 -0700

    allergy storing fixed

--- src/com/trydish/main/global.java
+++ src/com/trydish/main/global.java
@@ -1,6 +1,10 @@
 package com.trydish.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class global {
 	public static int userID = -1;
 	public static String username = "";
+	public static ArrayList<String> allergy_ids;
 }
